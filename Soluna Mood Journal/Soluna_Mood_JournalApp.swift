ce1bfb836479fd9a77a2972c26a4f2d30a555d6e
Lorenzo Viray <lviray2006@gmail.com>
2025-03-15 16:21:33 -0500

    Initial Commit

---
+++ Soluna Mood Journal/Soluna_Mood_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soluna_Mood_JournalApp.swift
+//  Soluna Mood Journal
+//
+//  Created by Lorenzo Viray on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Soluna_Mood_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
